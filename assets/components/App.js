c7bad235fe5d50058c68f85fffd563d536d7b81c
doananhkiet886 <doananhkiet0506@gmail.com>
2023-06-09 16:09:18 +0700

    fix import connect

--- assets/components/App.js
+++ assets/components/App.js
@@ -2,6 +2,7 @@ import html from '../build-ui/core.js'
 import Header from './Header.js';
 import TodoList from './TodoList.js';
 import Footer from './Footer.js';
+import { connect } from '../js/store.js'
 
 function App() {
     return html`
