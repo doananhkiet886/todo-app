187a1d2ce7325199270c81b36e4fb0c972cf9eb4
doananhkiet886 <doananhkiet0506@gmail.com>
2023-06-10 02:27:12 +0700

    #30 - handle hide footer when there is no todo

--- assets/components/App.js
+++ assets/components/App.js
@@ -4,11 +4,12 @@ import TodoList from './TodoList.js';
 import Footer from './Footer.js';
 import { connect } from '../js/store.js'
 
-function App() {
+function App({ todos }) {
+    console.log(todos);
     return html`
         ${Header()}
         ${TodoList()}
-        ${Footer()}
+        ${todos.length > 0 && Footer()}
     `
 }
 
